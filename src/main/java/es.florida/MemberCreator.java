000a430558bbe7a2ab7383f2e10b596cf2e74601
jordiGarcia81 <jordiGarcia81@users.noreply.github.com>
2020-12-03 05:35:15 +0100

    correos

--- src/main/java/es.florida/MemberCreator.java
+++ src/main/java/es.florida/MemberCreator.java
@@ -25,13 +25,16 @@ public class MemberCreator implements Runnable  {
     }
 
     private String generar(){
-        // Array de nombres
-        // Array Apellidos
-        // Escoer Nombre y apellidos aleatorio
-        // Array de correo o un correo fijo.
+        String[] nombres={"Jordi","Juan","Carlos","Jesus","Damia","Pablo","Rafa","Noe","Cristian","Raquel","Cristina","Javier","Enrique"};
+        String[] apellidos={"Garcia","Romero","Simon","Perez","Ruiz","Navarro","Gonzalez","Rodriguez","Martinez","Martin","Moreno","Alonso"};
+        String correo="testmailprueba2@gmail.com";
+        Random random=new Random();
+        int posNombre=random.nextInt(nombres.length);
+        int posApellido=random.nextInt(apellidos.length);
 
 
-        String currentName = ""; //Nombre Apellidos Correo
+
+        String currentName = "Enviando correo a: "+nombres[posNombre]+" "+apellidos[posApellido]+" "+correo;
         String output = currentName;
         return "\n"+output;
     }
