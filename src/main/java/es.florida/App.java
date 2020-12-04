0e777dadbe1179357a3b8cd2412dcd129e1e48c0
jordiGarcia81 <jordiGarcia81@users.noreply.github.com>
2020-12-04 06:03:03 +0100

    ExecutorService

--- src/main/java/es.florida/App.java
+++ src/main/java/es.florida/App.java
@@ -2,9 +2,15 @@ package es.florida;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+
+    private static ExecutorService executorService = Executors.newFixedThreadPool(42);
+    public static void main(String[] args) throws IOException, InterruptedException {
         MemberCreator memberCreator = new MemberCreator();
         MemberMonitor memberMonitor = new MemberMonitor();
         MailSender mailSender = new MailSender();
@@ -14,8 +20,56 @@ public class App {
         Thread memberMonitorThread = new Thread(memberMonitor);
         Thread mailSenderThread = new Thread(mailSender);
 
+
+
         memberCreatorThread.start();
         memberMonitorThread.start();
         mailSenderThread.start();
+
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        executorService.execute(new Thread(mailSender));
+        if(!executorService.awaitTermination(1, TimeUnit.SECONDS)){
+            executorService.shutdownNow();
+        }
     }
 }
